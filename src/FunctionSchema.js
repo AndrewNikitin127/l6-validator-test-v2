2bf2e88b00553a195056f76f0d97b4d305d8f770
AndrewNikitin127 <donandrain153@gmail.com>
2023-12-27 18:33:58 +0500

    fourth task complete

--- src/FunctionSchema.js
+++ src/FunctionSchema.js
@@ -1,21 +1,28 @@
 export default class FunctionSchema {
   validators = [(val) => val instanceof Function];
-  context = null;
+  context = this;
+  args = [];
 
-  constructor(newValidators, context){
+  constructor(newValidators, context, args){
     if (newValidators) this.validators = newValidators;
     if (context) this.context = context;
+    if (args) this.args = args;
   }
 
   isValid(val){
-    if (this.context !== null) val = val.bind(this.context);
+    if (this.context !== this) val = val.bind(this.context);
+    if (this.args.length !== 0) val = val.bind(this.context, this.args);
+
     return this.validators.every((validator) => validator(val));
   }
   expect(val){
     const validator = (func) => func() === val;
-    return new FunctionSchema ([].concat(this.validators, validator), this.context) // {...this.context} ? вроде не надо клонировать объект. так как мы привязываем конкретный объект в callWith. Но чет коробит от этого.
+    return new FunctionSchema ([...this.validators, validator], this.context, [...this.args]) // {...this.context} ? вроде не надо клонировать объект. так как мы привязываем конкретный объект в callWith. Но чет коробит от этого.
   }
   callWith(context){
-    return new FunctionSchema([].concat(this.validators), context);
+    return new FunctionSchema([...this.validators], context, [...this.args]);
+  }
+  arguments(args){
+    return new FunctionSchema([...this.validators], this.context, args);
   }
 }
