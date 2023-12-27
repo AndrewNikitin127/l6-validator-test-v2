c2cb9cbdf236b0cc0d25579afe8eca5c591ea109
AndrewNikitin127 <donandrain153@gmail.com>
2023-12-27 15:54:33 +0500

    second task complete

--- src/StringSchema.js
+++ src/StringSchema.js
@@ -1,7 +1,15 @@
 export default class StringSchema {
   validators = [(val) => typeof val === 'string'];
 
+  constructor(newValidators){
+    if (newValidators) this.validators = newValidators;
+  }
+
   isValid(val){
     return this.validators.every((validator) => validator(val));
+  }
+  hasSpaces(){
+    const validator = (string) => string.includes(' ');
+    return new StringSchema([].concat(this.validators, validator))
   }
 }
